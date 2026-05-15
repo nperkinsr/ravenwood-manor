0d2d6778c80e4baacaf97cfa45cd8b981feacbb6
Nelly Perkins <nellyperkins@gmail.com>
2026-05-15 16:26:29 +0200

    Added all the relic cards

--- scripts.js
+++ scripts.js
@@ -23,6 +23,7 @@ const spooksPerSecond = document.querySelector("#spook-rate-per-second span");
 const spooksPerClick = document.querySelector("#spook-rate-per-click span");
 const collectButton = document.querySelector("#collect-spooks-button");
 const relicsSection = document.querySelector("#relics-section");
+const relicList = document.querySelector(".relic-list");
 
 /////////////////////////////////////////////////////
 //////////       COUNTER DISPLAY      ///////////////
@@ -50,13 +51,58 @@ collectButton.addEventListener("click", collectSpooks);
 //////////          JSON FILE        ////////////////
 /////////////////////////////////////////////////////
 
-let gameData = {};
+let jsonObjects = {};
 
 async function loadGameData() {
   const jsonFile = await fetch("data.json");
   const jsonData = await jsonFile.json();
 
   jsonObjects = jsonData;
+  loadRelics();
 }
 
 loadGameData();
+
+/////////////////////////////////////////////////////
+//////////           RELICS          ////////////////
+/////////////////////////////////////////////////////
+
+function loadRelics() {
+  const relics = jsonObjects.relics;
+
+  relicList.innerHTML = "";
+
+  for (let relicIndex = 0; relicIndex < relics.length; relicIndex++) {
+    const relic = relics[relicIndex];
+    let relicMaxOwnedText = ""; // Ths an empty string for maxOwned text
+
+    // Check if maxOwned is defined for the relic
+    if (relic.maxOwned !== undefined) {
+      relicMaxOwnedText = `<span>0/${relic.maxOwned}</span>`; // Display "0/maxOwned" if maxOwned is defined
+    }
+
+    // The template to create the relics card HTML and add it to the relic list
+    relicList.innerHTML += `
+  <article class="relic-card">
+    <img class="relic-image" src="${relic.image}">
+    
+    <div class="relic-content">
+      <h3 class="relic-name">${relic.name}</h3>
+
+      <div class="relic-meta">
+        <span>${relic.baseCost} ${relic.currency}</span>
+        ${relicMaxOwnedText}
+      </div>
+
+      <p class="relic-description">
+        ${relic.description}
+      </p>
+    </div>
+
+    <button class="relic-buy-button" type="button">
+      Buy
+    </button>
+  </article>
+`;
+  }
+}
